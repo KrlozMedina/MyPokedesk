88c47bad266e0e5f98b54a96167ca15d366aa0ca
Carlos Medina <78941509+KrlozMedina@users.noreply.github.com>
2023-06-30 23:19:47 -0500

    [SERVER] New server

--- public/src/pokedesk.js
+++ public/src/pokedesk.js
@@ -2,7 +2,7 @@
 // const URL = 'https://pokemon-32ua.onrender.com';
 // const URL = 'http://localhost:8080';
 // const URL = "https://mypokedesk-production.up.railway.app"
-const URL = "https://app-8b35e64a-1873-4c5c-b0ac-0e2656fb85e4.cleverapps.io/"
+const URL = "https://app-8b35e64a-1873-4c5c-b0ac-0e2656fb85e4.cleverapps.io"
 const API = '/api/pokemon/';
 
 const pokedesk = document.querySelector(".pokedesk");
